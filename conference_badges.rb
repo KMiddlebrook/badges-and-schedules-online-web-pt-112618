ff6c7660d020b105a38d7443684cf5d109bab9af
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-12-06 16:10:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+  name = "Arel"
+      puts "Hello, my name is #{name}."
+  "Hello, my name is Arel."
+end
+
+def batch_badge_creator
+
+end
