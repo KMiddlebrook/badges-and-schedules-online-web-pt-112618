989543c8c5706dd46983d08a1b64472a1a27a4d3
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-12-07 12:24:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+  name = "Arel"
+      puts "Hello, my name is #{name}."
+  "Hello, my name is Arel."
+end
+
+def batch_badge_creator(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.each do |name|
+    puts "Hello, my name is #{name}."
+end
