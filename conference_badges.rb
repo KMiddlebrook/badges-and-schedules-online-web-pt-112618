05d5ac9964699eb20884f6bb83d7ac58c4437711
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-12-07 12:56:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require "pry"
+def badge_maker(name)
+  name = "Arel"
+      puts "Hello, my name is #{name}."
+  "Hello, my name is Arel."
+end
+
+def batch_badge_creator(speakers)
+  binding.pry
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms
+
+end
+
+def printer
+
+end
+
+
+batch_badge_creator
